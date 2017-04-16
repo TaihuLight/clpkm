2b22cbff761af139ae3c616bcc67cee6b7ee3cbc
TOCK Chiu <tock.chiu@gmail.com>
2017-04-16 20:48:21 +0800

    CLPKMPP seems to be functional now. It may still have some minior bugs.

--- pp/Main.cpp
+++ pp/Main.cpp
@@ -1,156 +1,127 @@
-//------------------------------------------------------------------------------
-// CLPKMCC
+//===----------------------------------------------------------------------===//
+// CLPKMPP
 //
 // Modified from Eli's tooling example
-//------------------------------------------------------------------------------
+//===----------------------------------------------------------------------===//
 #include <sstream>
 #include <string>
-#include <vector>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
 
 #include "clang/AST/AST.h"
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
-#include "clang/Analysis/CFG.h"
-#include "clang/Analysis/CallGraph.h"
 #include "clang/Frontend/ASTConsumers.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Rewrite/Core/Rewriter.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
+
+#define DEBUG_TYPE "clpkmpp"
 
 using namespace clang;
 using namespace clang::driver;
 using namespace clang::tooling;
 
-static llvm::cl::OptionCategory CLPKMCategory("CLPKM");
+static llvm::cl::OptionCategory CLPKMPPCategory("CLPKMPP");
 
-// By implementing RecursiveASTVisitor, we can specify which AST nodes
-// we're interested in by overriding relevant methods.
-class MyASTVisitor : public RecursiveASTVisitor<MyASTVisitor> {
+
+
+using CallSiteTable = std::unordered_map<FunctionDecl*, std::deque<CallExpr*>>;
+using RetStmtTable = std::unordered_map<FunctionDecl*, std::deque<ReturnStmt*>>;
+using CodeSnippet = std::deque<std::string>;
+using CodeCache = std::unordered_map<FunctionDecl*, CodeSnippet>;
+
+
+
+class Extractor : public RecursiveASTVisitor<Extractor> {
 public:
-	MyASTVisitor(Rewriter& R) : TheRewriter(R) {
+	Extractor(CallSiteTable& CST, RetStmtTable& RST) :
+		TheCST(CST), TheRST(RST), TheFunction(nullptr) {
 
 		}
 
-	bool VisitStmt(Stmt* s) {
+	bool TraverseFunctionDecl(FunctionDecl* FuncDecl) {
 
-		// Only care about If statements.
-		if (isa<IfStmt>(s)) {
-
-			IfStmt* IfStatement = cast<IfStmt>(s);
-			Stmt*   Then = IfStatement->getThen();
-			Stmt*   Else = IfStatement->getElse();
-
-			}
+		TheFunction = FuncDecl;
+		RecursiveASTVisitor<Extractor>::TraverseFunctionDecl(FuncDecl);
+		TheFunction = nullptr;
 
 		return true;
 
 		}
 
-	bool VisitFunctionDecl(FunctionDecl *FuncDecl) {
+	bool VisitReturnStmt(ReturnStmt* RS) {
 
-		if (FuncDecl == nullptr)
-			return false;
+		assert(TheFunction != nullptr && "ReturnStmt outside of function?");
+		TheRST[TheFunction].emplace_back(RS);
+		return true;
 
-		DeclarationName DeclName = FuncDecl->getNameInfo().getName();
-		std::string FuncName = DeclName.getAsString();
+		}
 
-		if (!FuncDecl->hasAttr<OpenCLKernelAttr>()) {
+	bool VisitCallExpr(CallExpr* CE) {
 
-			llvm::errs() << "Non-kernel function " << FuncName
-			             << " should be inlined first.\n";
-			return false;
-
-			}
-
-		size_t ParamSize = FuncDecl->param_size();
-		const char* CLPKMParam = ", __global char * __clpkm_hdr, "
-		                         "__global char * __clpkm_local, "
-		                         "__global char * __clpkm_lvb";
-
-		// FIXME: I can't figure out a graceful way to do this at the moment
-		//        It's uncommon anyway
-		if (ParamSize == 0)
-			llvm_unreachable("function w/ parameter is yet implemented.");
-
-		auto LastParam = FuncDecl->getParamDecl(ParamSize - 1);
-		auto InsertCut = LastParam->getSourceRange().getEnd();
-
-		TheRewriter.InsertTextAfterToken(InsertCut, CLPKMParam);
-
-		if (!FuncDecl->hasBody())
-			return true;
-
-		CFG::BuildOptions Options;
-		auto CFG = CFG::buildCFG(FuncDecl, FuncDecl->getBody(),
-		                         &FuncDecl->getASTContext(), Options);
-
-		llvm::errs() << "Dump CFG of funcion " << FuncName << '\n';
-		CFG->dump(TheRewriter.getLangOpts(), true);
-
+		assert(TheFunction != nullptr && "CallExpr outside of function?");
+		TheCST[TheFunction].emplace_back(CE);
 		return true;
 
 		}
 
 private:
-	Rewriter &TheRewriter;
+	CallSiteTable& TheCST;
+	RetStmtTable&  TheRST;
+
+	FunctionDecl*  TheFunction;
 
 	};
 
-// Implementation of the ASTConsumer interface for reading an AST produced
-// by the Clang parser.
-class MyASTConsumer : public ASTConsumer {
+
+
+class ExtractorDriver : public ASTConsumer {
 public:
-	MyASTConsumer(Rewriter &R) : Visitor(R) {
+	template <class ... P>
+	ExtractorDriver(P&& ... Param) :
+		Visitor(std::forward<P>(Param)...) {
 
 		}
 
 	bool HandleTopLevelDecl(DeclGroupRef DeclGroup) override {
 
-		for (auto& Decl : DeclGroup) {
-
-			if (!Decl->isInvalidDecl())
-				CallGraph.addToCallGraph(Decl);
-
-			}
-
-		CallGraphNode* VirtualRoot = CallGraph.getRoot();
-
-		llvm::errs() << "Dump virtual root\n";
-		VirtualRoot->print(llvm::errs());
-		llvm::errs() << '\n';
+		for (auto& Decl : DeclGroup)
+			Visitor.TraverseDecl(Decl);
 
 		return true;
 
 		}
 
-
 private:
-	CallGraph CallGraph;
-	MyASTVisitor Visitor;
+	Extractor Visitor;
 
 	};
 
 
 
-class CLPKMCCFrontendAction : public ASTFrontendAction {
+class CLPKMPPFrontendAction : public ASTFrontendAction {
 public:
-	CLPKMCCFrontendAction() = default;
+	CLPKMPPFrontendAction() = default;
 
 	void EndSourceFileAction() override {
 
-		SourceManager &SM = TheRewriter.getSourceMgr();
-		TheRewriter.getEditBuffer(SM.getMainFileID()).write(llvm::outs());
+		SourceManager &SM = PPRewriter.getSourceMgr();
+		PPRewriter.getEditBuffer(SM.getMainFileID()).write(llvm::outs());
 
 		}
 
 	std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
 	                                               StringRef file) override {
 
-		TheRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
-		return llvm::make_unique<MyASTConsumer>(TheRewriter);
+		PPRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
+		return llvm::make_unique<ExtractorDriver>(CST, RST);
 
 		}
 
@@ -161,8 +132,195 @@ public:
 		
 		}
 
+	void ExecuteAction() override {
+
+		ASTFrontendAction::ExecuteAction();
+		Nonce = 0;
+
+		while (!CST.empty()) {
+
+			auto It = CST.begin();
+			std::unordered_set<FunctionDecl*> CallChain;
+
+			PerformInline(It->first, CallChain);
+
+			}
+
+		CST.clear();
+
+		}
+
 private:
-	Rewriter TheRewriter;
+	Rewriter      PPRewriter;
+	CallSiteTable CST;
+	RetStmtTable  RST;
+	CodeCache     CC;
+	size_t        Nonce;
+
+	// Generate code snippet for inlining
+	bool GenCodeSnippet(FunctionDecl* FuncDecl, std::unordered_set<FunctionDecl*>& CallChain) {
+
+		// Already generated code snippet
+		if (CC.find(FuncDecl) != CC.end())
+			return true;
+
+		// Inline this function first and then generate code snippet
+		if (CST.find(FuncDecl) != CST.end() && !PerformInline(FuncDecl, CallChain))
+			return false;
+
+		auto RetStmtRecord = RST.find(FuncDecl);
+
+		// If this function got no return statement
+		if (RetStmtRecord == RST.end()) {
+
+			CodeSnippet Body;
+
+			Body.emplace_back(PPRewriter.getRewrittenText(FuncDecl->getBody()->getSourceRange()));
+			CC.emplace(FuncDecl, std::move(Body));
+
+			return true;
+
+			}
+
+		CodeSnippet CS;
+		SourceLocation Front = FuncDecl->getBody()->getLocStart();
+
+		for (ReturnStmt* RS : RetStmtRecord->second) {
+
+			SourceLocation RSLocStart = RS->getLocStart();
+			SourceLocation RSLocEnd = RS->getLocEnd();
+
+			CS.emplace_back(PPRewriter.getRewrittenText({Front, RSLocStart.getLocWithOffset(-1)}));
+			CS.emplace_back(PPRewriter.getRewrittenText({RSLocStart.getLocWithOffset(6), RSLocEnd}));
+			Front = RSLocEnd.getLocWithOffset(1);
+
+			}
+
+		CS.emplace_back(PPRewriter.getRewrittenText({Front, FuncDecl->getBody()->getLocEnd()}));
+		CC.emplace(FuncDecl, std::move(CS));
+
+		return true;
+
+		}
+
+	bool PerformInline(FunctionDecl* FuncDecl, std::unordered_set<FunctionDecl*>& CallChain) {
+
+		if (!FuncDecl->hasBody()) {
+
+			llvm::errs() << "Skipping declaration without body \""
+			             << FuncDecl->getNameInfo().getName().getAsString()
+			             << "\"\n";
+			return true;
+
+			}
+
+		if (CallChain.find(FuncDecl) != CallChain.end()) {
+
+			// TODO
+			auto& Diag = getCompilerInstance().getDiagnostics();
+
+			llvm::errs() << "Recursion detected.\n";
+			return false;
+
+			}
+
+		auto CallSiteRecord = CST.find(FuncDecl);
+
+		// No function call to inline or already inlined
+		if (CallSiteRecord == CST.end())
+			return true;
+
+		auto CallChainIt = CallChain.emplace(FuncDecl);
+		assert(CallChainIt.second && "Failed to emplace to CallChain");
+
+		// Clang traverse the AST in a DFS manner
+		// Subexpressions appear after the main expression, and they should
+		// be inlined before the main expression or we will lose the track of
+		// source code range
+		for (auto CEIt = CallSiteRecord->second.rbegin();
+		     CEIt != CallSiteRecord->second.rend();
+		     ++CEIt) {
+
+			CallExpr* CE = *CEIt;
+			FunctionDecl* Callee = CE->getDirectCallee();
+
+			// May be builtin function
+			if (!Callee->hasBody())
+				continue;
+
+			if (!GenCodeSnippet(Callee = Callee->getDefinition(), CallChain))
+				return false;
+
+			std::string ExitLabel = "__CLPKM_EXIT_" + std::to_string(Nonce++);
+			std::string Replace;
+			std::string RetVar = " __clpkm_ret_" + std::to_string(Nonce++);
+			QualType RetType = Callee->getReturnType();
+
+			auto CCRecord = CC.find(Callee);
+			assert(CCRecord != CC.end());
+
+			auto Next = CCRecord->second.begin();
+			auto It = Next;
+
+			// Patch all the return statements with do-while and goto
+			// Because the ';' after a return statement is not included in
+			// its source range, we need the good ol' trick of macro magic
+			while (++Next != CCRecord->second.end()) {
+
+				Replace += *It + "do { ";
+
+				if (!RetType->isVoidType())
+					Replace += RetVar + " = " + *Next + "; ";
+
+				Replace += "goto " + ExitLabel + "; } while (0)";
+
+				It = ++Next;
+
+				}
+
+			Replace += *It;
+
+			auto ParamIt = Callee->param_begin();
+			auto ArgIt = CE->arg_begin();
+
+			// Prepare the arguments for the call expression
+			while (ParamIt != Callee->param_end()) {
+
+				Replace = PPRewriter.getRewrittenText((*ParamIt)->getSourceRange()) +
+				          " = " +
+				          PPRewriter.getRewrittenText((*ArgIt)->getSourceRange()) +
+				          ";" + std::move(Replace);
+
+				++ArgIt;
+				++ParamIt;
+
+				}
+
+			// Statement expressions from GNU extension
+			if (!RetType->isVoidType())
+				Replace = RetType.getAsString() + RetVar + "; " +
+				          std::move(Replace) +
+				          ExitLabel + ": " + RetVar + "; ";
+			else
+				Replace = std::move(Replace) + ExitLabel + ": ; ";
+
+			// Locally declared labels from GNU extension
+			// Must be placed at the beginning of a block
+			Replace = "({ __label__ " + ExitLabel + "; " +
+			          std::move(Replace) + "})";
+
+			PPRewriter.ReplaceText(CE->getSourceRange(),
+			                       Replace);
+
+			}
+
+		// Remove the record so we won't inline twice
+		CST.erase(CallSiteRecord);
+		CallChain.erase(CallChainIt.first);
+
+		return true;
+
+		}
 
 	};
 
@@ -170,9 +328,9 @@ private:
 
 int main(int ArgCount, const char* ArgVar[]) {
 
-	CommonOptionsParser Options(ArgCount, ArgVar, CLPKMCategory);
+	CommonOptionsParser Options(ArgCount, ArgVar, CLPKMPPCategory);
 	ClangTool Tool(Options.getCompilations(), Options.getSourcePathList());
 
-	return Tool.run(newFrontendActionFactory<CLPKMCCFrontendAction>().get());
+	return Tool.run(newFrontendActionFactory<CLPKMPPFrontendAction>().get());
 
 	}
