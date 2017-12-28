816e1d2b2f1dc2663c42af3b94c7fe2c9ec555bd
TOCK Chiu <tock.chiu@gmail.com>
2017-12-28 21:33:33 +0800

    tools: add a tool to generate CFG PNG file from PTX assembly

---
+++ tools/ptx2png.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ ! "$#" -eq 2 ]; then
+	echo 'Usage:'
+	echo "  '$0' <input-ptx.s> <output.png>"
+	exit 1
+fi
+
+TMPVER=/tmp/ptx2png-"$BASHPID"-"$RANDOM".s
+TMPBIN=/tmp/ptx2png-"$BASHPID"-"$RANDOM".cubin
+TMPDOT=/tmp/ptx2png-"$BASHPID"-"$RANDOM".dot
+
+# Workaround old CUDA
+cp "$1" "$TMPVER"
+sed -i 's/\.version 6\.1/\.version 5\.0/g' "$TMPVER"
+
+# Compile to cubin
+/opt/cuda/bin/ptxas -arch=sm_35 "$TMPVER" -o "$TMPBIN"
+
+# Generate CFG
+/opt/cuda/bin/nvdisasm -cfg "$TMPBIN" > "$TMPDOT"
+sed -i 's/fontname=\"Courier\",fontsize=10/fontname=\"Monaco\",fontsize=18/g' "$TMPDOT"
+dot -o"$2" -Tpng < "$TMPDOT"
+
+# Clean up
+rm "$TMPVER" "$TMPBIN" # "$TMPDOT"
